23b3e216675e5ac5f18df9c882d99d6d60a92c0b
Nooorrrr <noor.bnhdd@gmail.com>
2024-02-22 22:44:04 +0100

    exo7

--- exo7.dart
+++ exo7.dart
@@ -1,11 +1,18 @@
 //Write a function in Dart called createUser with parameters name, age, and isActive, where isActive has a default value of true.
 
-void createUser(String name, int age, bool isActive) {
-  isActive ?? true;
-  print(
-      "User created with:\nName: $name\nAge: $age\nActivity Status: $isActive");
+class User {
+  String? name;
+  int? age;
+  bool? isActive;
+
+  User(String n, int a, bool bool);
+}
+
+User createUser(String n, int a) {
+  return User(n, a, true);
 }
 
 void main() {
-  createUser("Noor", 18,true );
+  createUser("Noor", 18);
+  
 }
